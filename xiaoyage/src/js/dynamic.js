a0e449c90de2cea5b9e5e10911dc59e426efc5b2
zhangjinpei <zhangjinpei@eking.mobi>
2016-01-22 02:30:41 +0800

    修改弹出框样式

--- xiaoyage/src/js/dynamic.js
+++ xiaoyage/src/js/dynamic.js
@@ -3,14 +3,17 @@
  */
 
 $('document').ready(function () {
+    //点击动态链接，弹出动态详情，点击关闭，收起弹窗;
     $('.dynamic-list .dynamic-link').on('click',function () {
-        $('.model-mask,.dynamic-model-content').fadeIn(300);
+        $('.dynamic-model').css({top: '0'});
+        $('.dynamic-top').css({overflow: 'initial'})
+        $('.footer').hide();
     });
+    $('.dynamic-model .model-close').on('click',function () {
+        $('.dynamic-model').css({top: '100%'});
+         $('.dynamic-top').css({overflow: 'hidden'});
+        $('.footer').show();
+    })
 
-    $('#showDetails').on('click',function () {
-        $('.model-mask,.model-content').fadeIn(300);
-    });
-    $('.model-mask,.close-model').on('click',function () {
-        $('.model-mask,.model-content,.dynamic-model-content').fadeOut(300);
-    });
+
 });
